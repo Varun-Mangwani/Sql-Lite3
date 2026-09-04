c1b8f19d3baefafe6973fc0045f9151fed01103f
Varun Mangwani <vpm.media.business@gmail.com>
2026-09-04 16:08:07 +0000

    updates

---
+++ Databse.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const sql = require('better-sqlite3')
+
+const app = express()
+
+const db = new sql("database.db")
+
+db.exec(`CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL
+)`)
+
+module.exports = db;
